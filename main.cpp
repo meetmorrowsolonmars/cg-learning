8363226ca62f07ea17ff38d5e2850e2ee7057add
Maxim Denisenko <meetmorrowsolonmars@gmail.com>
2023-09-22 01:41:52 +0300

    create opengl context

--- main.cpp
+++ main.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
-#include <thread>
 
 #include <SDL3/SDL.h>
-
-using namespace std::chrono_literals;
+#include <glad/glad.h>
 
 int main(int, char **)
 {
     const int SCREEN_WIDTH = 640;
     const int SCREEN_HEIGHT = 480;
-    const auto WINDOW_FLAGS = SDL_EVENT_WINDOW_SHOWN;
+    const auto WINDOW_FLAGS = SDL_WINDOW_OPENGL;
 
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
@@ -17,11 +15,34 @@ int main(int, char **)
         return -1;
     }
 
+    if (SDL_GL_LoadLibrary(nullptr) < 0)
+    {
+        std::cerr << "SDL could not load OpenGL. Error: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    // TODO: check error
+    // Request an OpenGL 4.5 context (should be core)
+    SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    // Also request a depth buffer
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
     SDL_Window *window = SDL_CreateWindow("CG Learning", SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_FLAGS);
 
     if (window == nullptr)
     {
         std::cerr << "SDL could not create window. Error: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    auto context = SDL_GL_CreateContext(window);
+    if (context == nullptr)
+    {
+        std::cerr << "SDL could not create OpenGL context. Error: " << SDL_GetError() << std::endl;
         return -1;
     }
 
